902aa1ad9710f87b429f5e96aca5103922a1efba
pvrgpt <rajaparthx148@gmail.com>
2026-09-15 11:03:12 +0000

    fixed __name__

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 #!/bin/bash
 def add(a,b):
     return a + b
-if __name == "__main__":
+if __name__ == "__main__":
     result = add(2,2)
     assert result == 4, "Math is broken!"
     print(f"Success 2 + 2 is {result}")
